1c48f7a547fa4441f3cda471fef83d71a397214c
fernandoC9600 <45076603+fernandoC9600@users.noreply.github.com>
2019-04-29 16:56:50 -0500

    Create Utilitiesfc.py

---
+++ Utilitiesfc.py
@@ -0,0 +1,48 @@
+
+# author: vlarobbyk
+# Description: this class contains utility functions developed by third persons
+
+from pygame import Rect
+
+class Utilities:
+    
+    def __init__(self):
+        print('Utilities loaded...')
+        
+    
+    def drawText(self, surface, text, color, rect, font, aa=False, bkg=None):
+        rect = Rect(rect)
+        y = rect.top
+        lineSpacing = -2
+
+        # get the height of the font
+        fontHeight = font.size("Tg")[1]
+
+        while text:
+            i = 1
+
+            # determine if the row of text will be outside our area
+            if y + fontHeight > rect.bottom:
+                break
+
+            # determine maximum width of line
+            while font.size(text[:i])[0] < rect.width and i < len(text):
+                i += 1
+
+            # if we've wrapped the text, then adjust the wrap to the last word      
+            if i< len(text): 
+                i = text.rfind(" ", 0, i) + 1
+
+            # render the line and blit it to the surface
+            if bkg:
+                image = font.render(text[:i], 1, color, bkg)
+                image.set_colorkey(bkg)
+            else:
+                image = font.render(text[:i], aa, color)
+
+            surface.blit(image, (rect.left, y))
+            y += fontHeight + lineSpacing
+
+            # remove the text we just blitted
+            text = text[i:]
+        return text
